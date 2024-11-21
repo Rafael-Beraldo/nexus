16e93245bb24642d0983d550d07549b95f848d77
Rafael Beraldo <123744124+Rafael-Beraldo@users.noreply.github.com>
2024-11-21 11:49:49 -0300

    Update message

--- frontend/src/components/ChatWidget/index.js
+++ frontend/src/components/ChatWidget/index.js
@@ -32,7 +32,7 @@ const ChatWidget = () => {
         {
           model: "ft:gpt-4o-mini-2024-07-18:personal:nexus2:ATc4FsGB",
           messages: [
-            { role: "system", content: "Você é um chatbot chamado nexus e responderá perguntas sobre o ecommerce da nexus store e somente dela" },
+            { role: "system", content: "Você é um assistente de e-commerce da Nexus Store." },
             ...messages.map((msg) => ({
               role: msg.sender === "user" ? "user" : "assistant",
               content: msg.text,
